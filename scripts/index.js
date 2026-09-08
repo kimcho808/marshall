f0679a47ec23698de33208c916bc2c82bf878647
kimcho808 <yooa2468@gmail.com>
2026-09-08 18:12:07 +0900

    실내 섹션 더보기 버튼

--- scripts/index.js
+++ scripts/index.js
@@ -10,17 +10,17 @@
 const bestSwiper = document.querySelector('.best_swiper');
 // console.log(bestSwiper);
 
-for(let i=0; i<9; i++){
+for(let i=0; i<10; i++){
     const bestSlide = document.createElement('div');
     bestSlide.classList.add('swiper-slide');
 
     bestSlide.innerHTML = `
         <div class="product_g">
             <a href="#" class="product_wrap">
-                <p class="product" style="background-image: url(${bestDB[i].product});"></p>
-                <h3>${bestDB[i].title}</h3>
+                <p class="product" style="background-image: url(${bestdb[i].product});"></p>
+                <h3>${bestdb[i].title}</h3>
             </a>
-            <button type="button" class="cart"><img src="./images/shopping.svg" alt=""></button>
+            <button type="button" class="cart"><img src="./images/shopping.svg" alt="장바구니 담기"></button>
             <a href="#" class="find">판매처 찾기 <img src="./images/img_arrow.svg" alt=""></a>
         </div>
     `;
@@ -29,12 +29,42 @@ for(let i=0; i<9; i++){
 }
 
 const best_swiper = new Swiper(bestSwiper,{
-    slidesPerView: 'auto',
+    slidesPerView: 4.5,
     spaceBetween:80,
     loop: true,
-    centeredSlides: false,
+    centeredSlides: true,
     navigation:{
         prevEl:'.best_prev',
         nextEl:'.best_next',
 	},
 }); // 플러그인 연결
+
+// room swiper
+const roomSwiper = document.querySelector('.room_swiper');
+// console.log(roomSwiper);
+
+for(let i=0; i<6; i++){
+    const roomSlide = document.createElement('div');
+    roomSlide.classList.add('swiper-slide');
+
+    roomSlide.innerHTML = `
+        <div class="product_g">
+            <a href="#" class="product_wrap">
+                <p class="product_img"><img src="${roomdb[i].product}" alt=""></img></p>
+                <div class="title_price">
+                    <h3>${roomdb[i].title}</h3>
+                    <p class="price">${roomdb[i].price}</p>
+                </div>
+            </a>
+            <button type="button" class="cart"><img src="./images/shopping.svg" alt="장바구니 담기"></button>
+            <a href="#" class="find">판매처 찾기 <img src="./images/img_arrow.svg" alt=""></a>
+        </div>
+    `
+
+    roomSwiper.children[0].appendChild(roomSlide);
+}
+
+const room_swiper = new Swiper(roomSwiper,{
+    slidesPerView: 4,
+    spaceBetween:29,
+})
--- scripts/marsall_db.js
+++ scripts/marsall_db.js
@@ -1,5 +1,5 @@
 // marshall_db.js
-const bestDB = [
+const bestdb = [
     {
         id: 1,
         product: './images/amp_5.png',
@@ -27,22 +27,66 @@ const bestDB = [
     },
     {
         id: 6,
-        product: './images/amp_2.png',
-        title:'1974x Handwired Combo',
+        product: './images/amp_5.png',
+        title:'AS50D Combo',
     },
     {
         id: 7,
-        product: './images/amp_2.png',
-        title:'1974x Handwired Combo',
+        product: './images/amp_4.png',
+        title:'Origin20 Head',
     },
     {
         id: 8,
-        product: './images/amp_2.png',
-        title:'1974x Handwired Combo',
+        product: './images/amp_1.png',
+        title:'JCM800 Modified',
     },
     {
         id: 9,
+        product: './images/amp_3.png',
+        title:'1959 Handwired Head',
+    },
+    {
+        id: 10,
         product: './images/amp_2.png',
         title:'1974x Handwired Combo',
+    },
+]
+
+const roomdb = [
+    {
+        id: 1,
+        product: './images/speaker_2.png',
+        title:'Acton III',
+        price:'₩ 460,000',
+    },
+    {
+        id: 2,
+        product: './images/s_speaker_1.png',
+        title:'STOCKWELL III',
+        price:'₩ 399,000',
+    },
+    {
+        id: 3,
+        product: './images/speaker_1.png',
+        title:'STANMORE IV',
+        price:'₩ 640,000',
+    },
+    {
+        id: 4,
+        product: './images/speaker_4.png',
+        title:'WOBURN II',
+        price:'₩ 950,000',
+    },
+    {
+        id: 5,
+        product: './images/s_speaker_2.png',
+        title:'EMBERTON III',
+        price:'₩ 289,000',
+    },
+    {
+        id: 6,
+        product: './images/speaker_3.png',
+        title:'ACTON IV',
+        price:'₩ 470,000',
     },
 ]
